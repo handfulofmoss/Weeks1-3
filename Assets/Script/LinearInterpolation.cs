dbfc47090c18ee8a98e60b3632e5ec6a4b233f3f
Mikayla Hubbard <hubbarmi@shernet.sheridancollege.ca>
2026-01-14 10:22:11 -0500

    Added an easing animationcurve

--- Assets/Script/LinearInterpolation.cs
+++ Assets/Script/LinearInterpolation.cs
@@ -5,6 +5,7 @@ public class LinearInterpolation : MonoBehaviour
     public Transform start;
     public Transform end;
     public float t = 0;
+    public AnimationCurve curve;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,6 +22,6 @@ public class LinearInterpolation : MonoBehaviour
             t = 0;
         }
 
-        transform.position = Vector2.Lerp(start.position, end.position, t);
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
     }
 }
