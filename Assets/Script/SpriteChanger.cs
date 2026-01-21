d935a1afd022a7e358588b09998b4fb8b03b0fa8
Mikayla Hubbard <hubbarmi@shernet.sheridancollege.ca>
2026-01-21 10:32:45 -0500

    created an array to store random sprites which are chosen on program start

--- Assets/Script/SpriteChanger.cs
+++ Assets/Script/SpriteChanger.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,19 +7,25 @@ public class SpriteChanger : MonoBehaviour
 
     public SpriteRenderer SpriteRenderer;
     public Color col;
+    public Sprite[] barrels;
+    public int randomNumber;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //PickARandomColor();
+        PickARandomSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Keyboard.current.anyKey.wasPressedThisFrame == true)
-        //{
-        //    //PickARandomColor();
-        //}
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            //PickARandomColor();
+            PickARandomSprite();
+        }
+
         //DONT WRITE IT THIS WAY- will read it as sprite pos at (0,0): SpriteRenderer.sprite.bounds.Contains(mousePos)
         //INSTEAD DO: SpriteRenderer.bounds.Contains(mousePos)
 
@@ -31,17 +38,25 @@ public class SpriteChanger : MonoBehaviour
             //Y: use col variable
             SpriteRenderer.color = col;
         }
-
         else
         {
             //N: set color to white
             SpriteRenderer.color = Color.white;
-
         }
     }
 
     void PickARandomColor()
     {
         SpriteRenderer.color = Random.ColorHSV();
+    }
+
+    void PickARandomSprite()
+    {
+        //get a random num between 0 & 2
+        randomNumber = Random.Range(0, barrels.Length);
+
+        //use that to set the sprite
+        SpriteRenderer.sprite = barrels[randomNumber];
+
     }
 }
